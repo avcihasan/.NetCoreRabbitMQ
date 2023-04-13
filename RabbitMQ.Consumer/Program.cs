c259ab0d9f6caa022732b93fcc5a93b60ca19146
Hasan Avcı <hsnavci7@gmail.com>
2023-04-14 00:46:55 +0300

    DirectExchange

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -2,28 +2,67 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-//Bağlantı Oluşturma
+#region Basic
+////Bağlantı Oluşturma
+//ConnectionFactory factory = new();
+//factory.Uri = new("amqps://dqmpnror:GjrEnPhUvvnz4LGpp_CneJ0HFQujHrTt@puffin.rmq2.cloudamqp.com/dqmpnror");
+
+////Bağlantı Aktifleştirme ve Kanal Açma
+//using IConnection connection = factory.CreateConnection();
+//using IModel channel = connection.CreateModel();
+
+////Queue Oluşturma
+//channel.QueueDeclare(queue: "First-Queue", exclusive: false); //Consumer ile publisher de kuyruklar birebir aynı olamlıdır
+
+////Queueya Mesaj Alma
+//BasicGetResult result = channel.BasicGet(queue: "First-Queue", false);
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: "First-Queue", false, consumer);
+//consumer.Received += (sender, e) =>
+//{
+//    // Kuyruğa gelen mesajların işlendiği yerdir 
+//    //e.Body : Kuyuruktaki mesajın verisini  bütünsel olarak  getirir
+//    //e.Body.Span ya da e.Body.ToArray() : Kuyruktaki  mesajın byte veririsini getirir
+
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+//Console.Read();
+#endregion
+
+
+#region DirectExchange
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://dqmpnror:GjrEnPhUvvnz4LGpp_CneJ0HFQujHrTt@puffin.rmq2.cloudamqp.com/dqmpnror");
 
-//Bağlantı Aktifleştirme ve Kanal Açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//Queue Oluşturma
-channel.QueueDeclare(queue: "First-Queue", exclusive: false); //Consumer ile publisher de kuyruklar birebir aynı olamlıdır
+//1.Adım
+//
+channel.ExchangeDeclare(exchange: "dirext-exchange-example", type: ExchangeType.Direct);
 
-//Queueya Mesaj Alma
-BasicGetResult result = channel.BasicGet(queue: "First-Queue", false);
+//2.Adım
+//
+string queueName = channel.QueueDeclare().QueueName;
+
+//3.Adım
+channel.QueueBind(
+    queue:queueName,
+    exchange: "dirext-exchange-example",
+    routingKey: "dirext-queue-example");
+
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "First-Queue", false, consumer);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer:consumer) ;
+
 consumer.Received += (sender, e) =>
 {
-    // Kuyruğa gelen mesajların işlendiği yerdir 
-    //e.Body : Kuyuruktaki mesajın verisini  bütünsel olarak  getirir
-    //e.Body.Span ya da e.Body.ToArray() : Kuyruktaki  mesajın byte veririsini getirir
-
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
 };
-
 Console.Read();
+
+#endregion
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,29 +1,50 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
+#region Basic
+////Bağlantı Oluşturma
+//ConnectionFactory factory = new();
+//factory.Uri = new("amqps://dqmpnror:GjrEnPhUvvnz4LGpp_CneJ0HFQujHrTt@puffin.rmq2.cloudamqp.com/dqmpnror");
 
-//Bağlantı Oluşturma
+////Bağlantı Aktifleştirme ve Kanal Açma
+//using IConnection connection = factory.CreateConnection();
+//using IModel channel = connection.CreateModel();
+
+////Queue Oluşturma
+//channel.QueueDeclare(queue: "First-Queue", exclusive: false); // queue adı First-Queue
+
+////Queueya Mesaj Gönderme
+////byte[] message =Encoding.UTF8.GetBytes("Merhaba");
+////channel.BasicPublish(exchange:"",routingKey:"First-Queue",body:message);
+////exchange:"" ile default olan direct exchange seçildi. Bu exchange rotueing key olarak kuyruk ismini aldığı için kuyurk ismi ilgili yere yazıldı.
+
+//for (int i = 0; i < 100; i++)
+//{
+//    Task.Delay(1000);
+//    byte[] message = Encoding.UTF8.GetBytes("Hi RabbitMQ" + i);
+//    channel.BasicPublish(exchange: "", routingKey: "First-Queue", body: message);
+//}
+//Console.Read();
+#endregion
+
+#region DirectExchange
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://dqmpnror:GjrEnPhUvvnz4LGpp_CneJ0HFQujHrTt@puffin.rmq2.cloudamqp.com/dqmpnror");
 
-//Bağlantı Aktifleştirme ve Kanal Açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//Queue Oluşturma
-channel.QueueDeclare(queue: "First-Queue", exclusive: false); // queue adı First-Queue
+channel.ExchangeDeclare(exchange:"dirext-exchange-example",type:ExchangeType.Direct);
 
-//Queueya Mesaj Gönderme
-//byte[] message =Encoding.UTF8.GetBytes("Merhaba");
-//channel.BasicPublish(exchange:"",routingKey:"First-Queue",body:message);
-//exchange:"" ile default olan direct exchange seçildi. Bu exchange rotueing key olarak kuyruk ismini aldığı için kuyurk ismi ilgili yere yazıldı.
-
-for (int i = 0; i < 100; i++)
+while (true)
 {
-    Task.Delay(1000);
-    byte[] message = Encoding.UTF8.GetBytes("Hi RabbitMQ" + i);
-    channel.BasicPublish(exchange: "", routingKey: "First-Queue", body: message);
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "dirext-exchange-example",
+        routingKey: "dirext-queue-example",
+        body:byteMessage
+        );
 }
-
-
-Console.Read();
+#endregion
